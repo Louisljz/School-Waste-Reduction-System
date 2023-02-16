41963788169d49f2a1dc312756c7c67f5f05d9ed
Louis JZ <louis.ljz08@gmail.com>
2023-02-16 14:08:38 +0700

    App Design, Add Comments and Finishing

--- GUI/main.py
+++ GUI/main.py
@@ -1,78 +1,94 @@
+# Imports
+# Paths
+import sys, os
+# GUI
 from tkinter import *
 from PIL import Image, ImageTk
-import sys, os
+# Computer Vision
 import cv2
-from keras.models import load_model
 import numpy as np
+# Load Keras Model
+from keras.models import load_model
+# YOLO object detection
 from ultralytics import YOLO
 from ultralytics.yolo.utils.plotting import Annotator
 
 # Disable scientific notation for clarity
 np.set_printoptions(suppress=True)
 
+# Check whether file is run as exe or python script
 if getattr(sys, 'frozen', False):
     folder_path = os.path.dirname(sys.executable)
 else:
     folder_path = os.path.dirname(__file__)
 
+# Set Up Folder Paths
 image_path = os.path.join(folder_path, 'images/')
 model_path = os.path.join(folder_path, 'models/')
 
-
+# Initialize and Configure Tkinter Window
 window = Tk()
-cap = cv2.VideoCapture(0)
-
 window.geometry("1152x700")
 window.configure(bg = "#FFFFFF")
 window.title('School Waste Reduction System')
 window.resizable(False, False)
+icon_img = PhotoImage(file=os.path.join(image_path, 'icon.png'))
+window.iconphoto(True, icon_img)
 
+# Initialize Webcam, YOLO and Keras Model with labels
+cap = cv2.VideoCapture(0)
 yolo = YOLO(os.path.join(model_path, 'yolov8n.pt'))
 model = load_model(os.path.join(model_path, "waste_classifier.h5"), compile=False)
 class_names = open(os.path.join(model_path, "labels.txt"), "r").readlines()
 
+# Display Waste Classifier Window
 def display_page1():
+    # Camera and AI Function
     def display_cam():
+        # Read Camera and Resize Frame
         _, frame = cap.read()
-
-        # Resize the raw image into (224-height,224-width) pixels
         image = cv2.resize(frame, (224, 224), interpolation=cv2.INTER_AREA)
 
-        # Make the image a numpy array and reshape it to the models input shape.
+        # Reshape image to the model's input shape.
         image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
 
-        # Normalize the image array
+        # Normalize the image
         image = (image / 127.5) - 1
 
-        # Predicts the model
+        # Model Prediction
         prediction = model.predict(image, verbose=0)
-        index = np.argmax(prediction)
-        class_name = class_names[index]
-        name = class_name[2:].strip()
-        if name != 'Background':
+        index = np.argmax(prediction) # Get Label Index
+        class_name = class_names[index] # Get Label String
+        name = class_name[2:].strip() # Remove \n
+        # Display Prediction Only If there is Object
+        if name != 'Background': 
             message.set("AI classifies this Trash as: ")
             label.set(name)
         else:
             message.set('')
             label.set('')
         
+        # Set Webcam Image in Tkinter Label
         cv2image = cv2.resize(frame, (540, 380))
         cv2image = cv2.cvtColor(cv2image, cv2.COLOR_BGR2RGB)
         img = Image.fromarray(cv2image)
         imgtk = ImageTk.PhotoImage(image = img)
         cam.imgtk = imgtk
         cam.configure(image=imgtk)
-        
+        # Updates Frame
         cam.after(20, display_cam)
     
+    # Creates New Window
     page1 = Toplevel()
     page1.geometry('1152x700')
     page1.title('Canteen Waste Classifier')
 
-    bgimg = PhotoImage(file = os.path.join(image_path, "image_2.png"))
+    # Display Background
+    bgimg = PhotoImage(file = os.path.join(image_path, "bg2.png"))
     bg = Label(page1, image = bgimg)
     bg.place(x = 0, y = 0)
 
+    # Message and Prediction Display
     message = StringVar()
     message_label = Label(page1, textvariable=message, font=("Arial", 25), 
                             background='green', foreground='white')
@@ -83,68 +99,79 @@ def display_page1():
                             background='blue', foreground='white')
     class_label.place(x=770, y=350)
 
+    # Webcam Display
     cam = Label(page1)
     cam.place(x = 100, y = 180)
 
     display_cam()
     page1.mainloop()
 
+# Display Classroom Lighting Window
 def display_page2():
+    # Make variable global, to prevent UnboundLocalError
     global duration
-    duration = 0
+    duration = 0 # For time interval between Lights ON/OFF 
+    # Camera and AI Function
     def display_cam():
         global duration
-        counter = 0
+        counter = 0 # Counts the number of people in every frame
         _, img = cap.read()
     
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
+        # YOLO Prediction
         results = yolo.predict(img)
 
         for r in results:
-            
             annotator = Annotator(img)
             
             boxes = r.boxes
             for box in boxes:
-                b = box.xyxy[0]  # get box coordinates in (top, left, bottom, right) format
-                cls = yolo.names[int(box.cls)]
-                conf = box.conf[0]
+                b = box.xyxy[0] # Get Bbox Coordinates
+                cls = yolo.names[int(box.cls)] # Get Label String
+                conf = box.conf[0] # Get confidence score
                 if cls == 'person' and conf > 0.5:
-                    counter += 1
-                    annotator.box_label(b, color=(0, 255, 0))
-            
-        frame = annotator.result() 
+                    counter += 1 # Increments number of person
+                    # Drawing bounding box
+                    annotator.box_label(b, color=(0, 255, 0)) 
+        
+        frame = annotator.result() # Return annotated image
+        # Set Webcam Image in Tkinter Label
         cv2image = cv2.resize(frame, (540, 380))
         img = Image.fromarray(cv2image)
         imgtk = ImageTk.PhotoImage(image = img)
         cam.imgtk = imgtk
         cam.configure(image=imgtk)
-
+        
         people_counter.set(f"Number of People in Classroom: {counter}")
-
+        # If there is at least one person in classroom
         if counter >= 1:
+            # Increments duration of the person staying in the room
             if duration < 30:
                 duration += 1
-        else:
+        else: # If there is no person in classroom
             if duration > 0:
+                # Decrements duration of the person leaving the room
                 duration -= 1
-        
+        # Wait for some time, then turn on lights
         if duration == 30:
             light_status.set('Lights: ON')
+        # Wait for some time, then turn off lights
         elif duration == 0:
             light_status.set('Lights: OFF')
         
-        cam.after(20, display_cam)
+        cam.after(20, display_cam) # Updates Frame
 
+    # Creates New Window
     page2 = Toplevel()
     page2.geometry('1152x700')
     page2.title('Classroom Lighting System')
 
-    bgimg = PhotoImage(file = os.path.join(image_path, "image_3.png"))
+    # Display Background
+    bgimg = PhotoImage(file = os.path.join(image_path, "bg3.png"))
     bg = Label(page2, image = bgimg)
     bg.place(x = 0, y = 0)
 
+    # People Counter and Light ON/OFF Display
     people_counter = StringVar()
     counter_label = Label(page2, textvariable=people_counter, font=("Arial", 20), 
                             background='green', foreground='white')
@@ -155,13 +182,15 @@ def display_page2():
                             background='blue', foreground='white')
     status_label.place(x=750, y=350)
 
+    # Webcam Display
     cam = Label(page2)
     cam.place(x = 100, y = 180)
 
     display_cam()
     page2.mainloop()
 
-
+# Tkinter Main Window (Home Page)
+# Main Canvas
 canvas = Canvas(
     window,
     bg = "#FFFFFF",
@@ -171,43 +200,46 @@ canvas = Canvas(
     highlightthickness = 0,
     relief = "ridge"
 )
-
 canvas.place(x = 0, y = 0)
-image_image_1 = PhotoImage(
-    file=os.path.join(image_path, "image_1.png"))
-image_1 = canvas.create_image(
+
+# Background
+bg_img = PhotoImage(
+    file=os.path.join(image_path, "bg1.png"))
+bg = canvas.create_image(
     576.0,
     350.0,
-    image=image_image_1
+    image=bg_img
 )
 
-button_image_1 = PhotoImage(
-    file=os.path.join(image_path, "button_1.png"))
-button_1 = Button(
-    image=button_image_1,
-    borderwidth=0,
+# Button To Waste Classifier Window
+btn1_img = PhotoImage(
+    file=os.path.join(image_path, "btn1.png"))
+btn1 = Button(
+    image=btn1_img,
+    borderwidth=8,
     highlightthickness=0,
     command=display_page1,
-    relief="flat"
+    relief=RAISED
 )
-button_1.place(
-    x=636.0,
+btn1.place(
+    x=260.0,
     y=258.0,
     width=256.0,
     height=250.0
 )
 
-button_image_2 = PhotoImage(
-    file=os.path.join(image_path, "button_2.png"))
-button_2 = Button(
-    image=button_image_2,
-    borderwidth=0,
+# Button To Classroom Lighting Window
+btn2_img = PhotoImage(
+    file=os.path.join(image_path, "btn2.png"))
+btn2 = Button(
+    image=btn2_img,
+    borderwidth=8,
     highlightthickness=0,
     command=display_page2,
-    relief="flat"
+    relief=RAISED
 )
-button_2.place(
-    x=260.0,
+btn2.place(
+    x=636.0,
     y=258.0,
     width=256.0,
     height=250.0
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # School-Waste-Reduction-System
+
+## Overview
+This software is a monitoring system designed to reduce waste and save energy in a school. The code imports various libraries for image processing, including OpenCV, NumPy, and PIL. It also imports a pre-trained model for waste classification using Keras and a YOLO object detection model for counting the number of people in a classroom.
+
+## People Counter
+The folder '/People Counter' contains the code for YOLOv8 Object Detection, which will draw a bounding box around a person in the webcam image.
+For more information about YOLOv8, please refer to the following documentation from Ultralytics: https://docs.ultralytics.com/tasks/detection/
+
+## Waste Classifier
+The folder '/Waste Classifier' contains the teachable machine file ('AI Waste Classifier.tm'), which already contain the dataset for training. This file can be viewed by opening it from this website: https://teachablemachine.withgoogle.com/train/image
+The dataset has been taken from Trashnet: https://github.com/garythung/trashnet
+
+'/Evaluation Metrics' contains the Metrics evaluated from testing data. 
+'image.py' will classify the images from '/testing_images'. While 'webcam.py' will classify images directly from webcam stream. 
+
+## GUI
+### Set Up
+Before running 'GUI/main.py', be sure to run 'pip install -r requirements.txt', to install all the neccessary packages/dependencies needed by the program. 
+
+The GUI folder has '/images' and '/models', which are the assets needed to run the program. 
+
+### General Idea
+The program has two main functions: display_page1() and display_page2(). The first function is responsible for displaying a window that shows the webcam feed and uses the Keras model to classify the type of waste in the image. The second function displays a window that shows the webcam feed and uses the YOLO object detection model to count the number of people in a classroom, and decide whether to turn the light ON or OFF.
+
+### Waste Classifier Function
+In the first function, the program reads from the webcam and resizes the captured image to 224x224, which is the input shape for the Keras model. It then normalizes the image and makes a prediction using the pre-trained Keras model. If the model detects an object in the image (i.e., not a "Background"), the predicted label is displayed in a label widget in the window. The program updates the webcam image in the window every 20 milliseconds using the display_cam() function.
+
+### Lighting System Function
+In the second function, the program reads from the webcam and uses the YOLO object detection model to detect people in the image. It then counts the number of people in the image and displays the count in a label widget in the window. The program updates the webcam image in the window using the display_cam() function, and if it detects at least one person in the image, it toggles the classroom lights ON at a fixed time interval.
